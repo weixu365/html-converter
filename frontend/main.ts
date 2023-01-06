86f10f23ba5513b36957b5a4f22e3b9f88a71978
Wei Xu <wxu@seek.com.au>
2023-01-07 00:09:13 +1100

    Invoke webassembly from js

--- frontend/main.ts
+++ frontend/main.ts
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import * as md from "../pkg/md_to_html"
+
+console.log(md.md_to_html("## test"))
 createApp(App).mount('#app')
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use pulldown_cmark::{Parser, Options, html};
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
@@ -21,7 +22,25 @@ pub fn main_js() -> Result<(), JsValue> {
 
 
     // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    console::log_1(&JsValue::from_str("WASM started!"));
 
     Ok(())
+}
+
+#[wasm_bindgen]
+pub fn md_to_html(md: &str) -> Result<String, JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+
+    // Your code goes here!
+    console::log_1(&JsValue::from_str(md));
+
+    let parser = Parser::new_ext(md, Options::ENABLE_STRIKETHROUGH);
+    
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
+    Ok(html_output)
 }
